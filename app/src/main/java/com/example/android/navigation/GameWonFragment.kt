76acc43162a599d9e5d95803825e91f9b6d9e1ef
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2022-10-08 01:39:15 +0600

    Added intents

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,10 +16,9 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -38,11 +37,39 @@ class GameWonFragment : Fragment() {
             view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
 
-        var args = GameWonFragmentArgs.fromBundle(arguments!!)
-        Toast.makeText(context,
-            "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
-            Toast.LENGTH_LONG).show()
+        setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+        // check if activity resolves
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)){
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+
+    private fun getShareIntent(): Intent {
+        var args = GameWonFragmentArgs.fromBundle(requireArguments())
+        //Another way doesn't work
+        val shareIntent = Intent(Intent.ACTION_SEND) //WHY INTENT IN ANOTHER INTENT
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT,
+                getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+
+        return shareIntent
+    }
+
+    private fun shareSuccess(){
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item!!.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
